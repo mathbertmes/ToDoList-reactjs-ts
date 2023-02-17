4d9f9543e9291e0ef0f7e521e4e6174911efeeb1
Matheus Bertemes <mathbertemes@gmail.com>
2023-02-17 00:11:27 -0300

    mark task complete

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,12 +1,21 @@
 import { Circle, Trash } from "phosphor-react"
+import { useState } from "react"
 
 import styles from "./Task.module.css"
 
 export function Task(){
+
+  const [checkTaskComplete, setCheckTaskComplete] = useState(false)
+
+  function handleMarkCompleteTask(){
+    setCheckTaskComplete(!checkTaskComplete)
+  }
+
+
   return(
     <div className={styles.taskWrapper}>
       <div className={styles.markAndText}>
-        <input checked={false} type="checkbox" className={styles.markCompleteButton} />
+        <input  onClick={handleMarkCompleteTask} checked={checkTaskComplete} type="checkbox" className={styles.markCompleteButton} />
         <p>Bdhbeklfblshfblsf fhbfgehfgeufgh kdhfbkeb bcdhbfjdvbfiw jhfwehjf egeg ufdgeig eugdry3e yuerg3 dbie ueghdfe </p>
       </div>
       <button className={styles.trashButton}>
